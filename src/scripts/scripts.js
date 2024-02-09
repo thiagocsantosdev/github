1daa2b2091ab5344b9f869d8c452e8fde13e6268
Thiago C Santos <thiagocsantos.dev@gmail.com>
2024-02-08 22:13:33 -0300

    impletado melhorias na secao market, agora os symbols e name das coins são dinamicas seguindo o padrão correto de uso da api.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -6,15 +6,33 @@ const solValue = document.getElementsByClassName('sol-value')
 const adaValue = document.getElementsByClassName('ada-value')
 const dogValue = document.getElementsByClassName('dog-value')
 
+const btcVolume = document.getElementsByClassName('btcVolume')
+
 
 
 const btcChange24 = document.getElementById('btcChange24')
 const ethChange24 = document.getElementById('ethChange24')
 const xrpChange24 = document.getElementById('xrpChange24')
 
-// Market
+// symbol
 
 
+const rankOneSymbol = document.getElementsByClassName('rankOneSymbol')
+const rankTwoSymbol = document.getElementsByClassName('rankTwoSymbol')
+const rankThreeSymbol = document.getElementsByClassName('rankThreeSymbol')
+const rankFourSymbol = document.getElementsByClassName('rankFourSymbol')
+const rankFiveSymbol = document.getElementsByClassName('rankFiveSymbol')
+const rankSixSymbol = document.getElementsByClassName('rankSixSymbol')
+
+
+// Name
+
+const rankOneName = document.getElementsByClassName('rankOneName')
+const rankTwoName = document.getElementsByClassName('rankTwoName')
+const rankThreeName = document.getElementsByClassName('rankThreeName')
+const rankFourName = document.getElementsByClassName('rankFourName')
+const rankFiveName = document.getElementsByClassName('rankFiveName')
+const rankSixName = document.getElementsByClassName('rankSixName')
 
 
 
@@ -63,6 +81,11 @@ fetch(apiBtc)
     const priceAdaUsd= data.data[8].price_usd
     const priceDogUsd= data.data[10].price_usd
 
+
+    //Volume of criptos
+
+    const volumeBtcUsd = data.data[0].market_cap_usd
+
    //Price change 24h
     const btcChange24hdata = data.data[0].percent_change_24h
     const ethChange24hdata = data.data[2].percent_change_24h
@@ -77,6 +100,120 @@ fetch(apiBtc)
     const solBRL = priceSolUsd * dolarAtual
     const adaBRL = priceAdaUsd * dolarAtual
     const dogBRL = priceDogUsd * dolarAtual
+
+    //Transform VOLUME USD TO BRL
+
+    const volumeBRL = volumeBtcUsd * dolarAtual
+
+
+
+
+
+
+
+    //Symbol Coins =========================================
+    // === Rank1
+    rankOneSymboldata = data.data[0].symbol
+
+    for ( elements of rankOneSymbol){
+      elements.textContent = `${rankOneSymboldata}`
+    }
+
+    // === Rank2
+    rankTwoSymboldata = data.data[1].symbol
+
+    for ( elements of rankTwoSymbol){
+      elements.textContent = `${rankTwoSymboldata}`
+    }
+    
+    // === Rank3
+    rankThreeSymboldata = data.data[2].symbol
+
+    for ( elements of rankThreeSymbol){
+      elements.textContent = `${rankThreeSymboldata}`
+    }
+    
+    // === Rank4
+    rankFourSymboldata = data.data[3].symbol
+
+    for ( elements of rankFourSymbol){
+      elements.textContent = `${rankFourSymboldata}`
+    }
+
+    
+    // === Rank5
+    rankFiveSymboldata = data.data[4].symbol
+
+    for ( elements of rankFiveSymbol){
+      elements.textContent = `${rankFiveSymboldata}`
+    }
+
+    
+    // === Rank6
+    rankSixSymboldata = data.data[5].symbol
+
+    for ( elements of rankOneSymbol){
+      elements.textContent = `${rankOneSymboldata}`
+    }
+
+   
+//Name Coins =========================================
+       // === Rank1
+
+       rankOneNameData = data.data[0].name 
+       
+       for (elements of rankOneName){
+        elements.textContent = `${rankOneNameData}`
+
+       }
+
+        // === Rank2
+
+        rankTwoNameData = data.data[1].name 
+       
+        for (elements of rankTwoName){
+         elements.textContent = `${rankTwoNameData}`
+ 
+        }
+
+         // === Rank3
+
+       rankThreeNameData = data.data[2].name 
+       
+       for (elements of rankThreeName){
+        elements.textContent = `${rankThreeNameData}`
+
+       }
+
+        // === Rank1
+
+        rankFourNameData = data.data[3].name 
+       
+        for (elements of rankFourName){
+         elements.textContent = `${rankFourNameData}`
+ 
+        }
+
+         // === Rank1
+
+       rankFiveNameData = data.data[4].name 
+       
+       for (elements of rankFiveName){
+        elements.textContent = `${rankFiveNameData}`
+
+       }
+
+        // === Rank1
+
+        rankSixNameData = data.data[5].name 
+       
+        for (elements of rankSixName){
+         elements.textContent = `${rankSixNameData}`
+ 
+        }
+
+
+     
     
 
 
@@ -149,6 +286,13 @@ fetch(apiBtc)
     dogElement.textContent = `R$: ${dogBRL.toFixed(2)}`;
   }
 
+
+  // Volume
+
+  
+  for (const btcVolumeElement of btcVolume){
+    btcVolumeElement.textContent = `R$: ${volumeBRL.toFixed(2)}`;
+  }
 
 
 
